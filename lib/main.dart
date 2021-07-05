8a99151ff9a4e2b9bbb36d55dddb1f26666db63b
viswamvs <viswavickymvs@gmail.com>
2021-07-05 19:59:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    color: Colors.yellow,
+    child: Center(
+      child: Text(
+        'Hello flutter',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Times New Roman',
+          fontSize: 26,
+          fontStyle: FontStyle.italic,
+        ),
+      ),
+    ),
+  ));
+}
