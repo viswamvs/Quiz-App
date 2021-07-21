37e9e968c4668cfac6480fc76d89339cbd2b0107
viswamvs <viswavickymvs@gmail.com>
2021-07-21 18:45:02 +0530

    added background color for appbar and UI

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
         appBar: AppBar(
           title: Text('Hello flutter'),
-          backgroundColor: Colors.lightBlue,
+          backgroundColor: Colors.lightGreen,
         ),
         body: Center(
           child: Text('Good Evening'),
