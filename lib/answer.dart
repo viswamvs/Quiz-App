da92a78f86294bbfae06fc0559c6058cb30056d3
viswamvs <viswavickymvs@gmail.com>
2021-07-30 20:50:22 +0530

    added another argument in constructor for accepting the answers

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
+  final String answerText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,7 +13,7 @@ class Answer extends StatelessWidget {
       child: RaisedButton(
         color: Colors.blue,
         onPressed: selectHandler,
-        child: Text('Answer 1'),
+        child: Text(answerText),
       ),
     );
   }
